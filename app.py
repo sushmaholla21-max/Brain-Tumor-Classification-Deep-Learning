66397065a29b90556560665737eaa08b0252e679
sushmaholla21-max <sushmaholla21@gmail.com>
2026-07-19 15:37:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -28,10 +28,8 @@ def load_pytorch_model():
     model = models.resnet101(weights=None) # Change to models.vgg19 if you used VGG19
     num_ftrs = model.fc.in_features
     model.fc = nn.Sequential(
-        nn.Linear(num_ftrs, 512),
-        nn.ReLU(),
         nn.Dropout(0.5),
-        nn.Linear(512, 4)
+        nn.Linear(num_ftrs, 4)
     )
     
     model.load_state_dict(torch.load(MODEL_PATH, map_location=torch.device('cpu')))
